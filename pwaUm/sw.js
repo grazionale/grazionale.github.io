c2a75245433f01d2fdf828a4f7e8dde071e13209
Grazionale <gabrielgrazionale@hotmail.com>
2018-10-09 16:41:16 -0300

    ba

--- pwaUm/sw.js
+++ pwaUm/sw.js
@@ -6,10 +6,30 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
-        '/',
-        'index.html',
-        'styles.css',
-        'manifest.js'
+        '/pwaUm/',
+        '/pwaUm/index.html',
+        '/pwaUm/manifest.js',
+        '/pwaUm/style/bootstrap/dist/css/bootstrap.min.css',
+        '/pwaUm/style/font-awesome/css/font-awesome.min.css',
+        '/pwaUm/style/Ionicons/css/ionicons.min.css',
+        '/pwaUm/dist/css/AdminLTE.min.css',
+        '/pwaUm/dist/css/skins/all-skins.min.css',
+        '/pwaUm/style/style.css',
+        '/pwaUm/dist/css/skins/all-skins.min.css',
+        '/pwaUm/style/jquery/dist/jquery.min.js',
+        '/pwaUm/dist/js/jquery-ui/jquery-ui.min.js',
+        '/pwaUm/dist/js/adminlte.min.js',
+        '/pwaUm/dist/js/pages/dashboard.js',
+        '/pwaUm/dist/js/demo.js',
+        '/pwaUm/style/bootstrap/dist/js/bootstrap.min.js',
+        '/pwaUm/dist/js/loadJson.js',
+        '/pwaUm/dist/js/parana.js',
+        '/pwaUm/dist/js/generator.js',
+        '/pwaUm/dist/js/counters.js',
+        '/pwaUm/dist/js/graph.js',
+        '/pwaUm/dist/js/filters.js',
+        '/pwaUm/dist/js/functions.js',
+        '/pwaUm/dist/js/utils.js',
       ]);
     })
   )
@@ -37,3 +57,4 @@ self.addEventListener('fetch', function (event) {
     })
   );
 });
+
